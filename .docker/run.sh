bbf9af272cdb94b2b56cf53b5dd5d8b3499da3d9
jefferson-norberto2 <jmn@cin.ufpe.br>
2026-08-05 09:04:47 -0300

    FEAT: Add docker files

--- .docker/run.sh
+++ .docker/run.sh
@@ -9,4 +9,4 @@ docker run -d -it \
   -e QT_X11_NO_MITSHM=1 \
   --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
   --device /dev/dri:/dev/dri \
-  kortex_jazzy:1.2.1
+  kortex_jazzy:<version>
