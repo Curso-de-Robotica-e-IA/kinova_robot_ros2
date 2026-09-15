12f8c38ec8e6d410c754c4dbb75ea6d5a1ce831e
jefferson-norberto2 <jmn@cin.ufpe.br>
2026-09-15 08:51:48 -0300

    FIX: Using moveit_servo as dependence

--- .docker/run.sh
+++ .docker/run.sh
@@ -9,4 +9,4 @@ docker run -d -it \
   -e QT_X11_NO_MITSHM=1 \
   --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
   --device /dev/dri:/dev/dri \
-  kortex_humble:latest
+  kortex_humble:1.0.1
